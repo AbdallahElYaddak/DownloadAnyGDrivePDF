7b7d2d405117a978cbea14d983d5dc6dd291b297
Abdallah Ali <40410142+AbdallahElYaddak@users.noreply.github.com>
2021-03-13 02:24:50 +0200

    Create FBLinksToPC.js

---
+++ FBLinksToPC.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Download Google Drive PDFs
+// @namespace    http://tampermonkey.net/
+// @updateURL    https://github.com/AbdallahElYaddak/DownloadAnyGDrivePDF/blob/main/FBLinksToPC.js
+// @version      0.1
+// @description  Changes FB Mobile links to PC links.
+// @author       Abdallah Khaled El-Yaddak
+// @match        https://m.facebook.com/*
+// ==/UserScript==
+
+if (window.location.href.indexOf("m.facebook.com")>-1){
+    if (window.location.href.indexOf("m.facebook.com/friends/center/friends")==-1){
+        var a = window.location.href;
+        if (a.includes("story.php?")){
+            var p2 = a.substring(a.search('story_fbid='), a.search('&'))
+            var p1 = a.substring(a.search('&id=')+1,getPosition(a,"?",2))
+            window.location.replace("https://www.facebook.com/story.php?" + p1 + "&" + p2)
+        } else {
+            a = a.replace("https://m.facebook.com/", "https://www.facebook.com/");
+            window.location.replace(a);
+        }
+
+    }
+}
+function getPosition(string, subString, index) {
+    return string.split(subString, index).join(subString).length;
+}
