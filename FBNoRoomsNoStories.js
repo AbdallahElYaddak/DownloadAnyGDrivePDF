c269f12ac4a65e740d4c3d7efa2d7818ff8c4471
Abdallah Ali <40410142+AbdallahElYaddak@users.noreply.github.com>
2022-02-15 01:20:43 +0200

    Update FBNoRoomsNoStories.js

--- FBNoRoomsNoStories.js
+++ FBNoRoomsNoStories.js
@@ -2,16 +2,19 @@
 // @name         Remove rooms & stories
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Remove FB Stories & VideoRooms
+// @description   Remove FB Stories & VideoRooms
 // @author       You
 // @match        https://www.facebook.com/?ref=home
+// @match        https://www.facebook.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=facebook.com
 // @grant        none
 // ==/UserScript==
 
 window.onload = setInterval(function() {
     'use strict';
-    document.querySelectorAll("[data-pagelet='VideoChatHomeUnit'")[0].parentElement.parentElement.remove();
-    document.querySelectorAll("[data-pagelet='Stories'")[0].remove();
+    try{document.querySelectorAll("[data-pagelet='VideoChatHomeUnit'")[0].parentElement.parentElement.remove()}
+    catch (ex){console.log.LogError(ex);};
+    try{document.querySelectorAll("[data-pagelet='Stories'")[0].remove()}
+    catch (ex){console.log.LogError(ex);};
     // Your code here...
 }, 2000);
